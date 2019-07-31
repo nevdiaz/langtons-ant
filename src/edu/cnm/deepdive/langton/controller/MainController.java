4980a1ec57ec2df31676f32f15c43d4e2899ce31
nevdiaz <jenndiazz@icloud.com>
2019-07-31 09:29:10 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/langton/controller/MainController.java
@@ -0,0 +1,20 @@
+package edu.cnm.deepdive.langton.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleButton;
+
+public class MainController {
+
+  @FXML
+  private ToggleButton runToggle;
+
+  @FXML
+  private Slider populationSize;
+
+  @FXML
+  private void toggleRun(ActionEvent actionEvent) {
+
+  }
+}

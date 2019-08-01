cab5509779d0a0055d15e60fc66a969e3272f612
nevdiaz <jenndiazz@icloud.com>
2019-08-01 09:34:09 -0600

    added button, new icon

--- src/edu/cnm/deepdive/langton/Main.java
+++ src/edu/cnm/deepdive/langton/Main.java
@@ -6,12 +6,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
   private static final String RESOURCES_DIR = "resources";
   private static final String LAYOUT_RESOURCE = RESOURCES_DIR + "/main.fxml";
+  private static final String ICON_RESOURCE = RESOURCES_DIR + "/ant.jpg";
   private static final String RESOURCE_BUNDLE_BASE_NAME = RESOURCES_DIR + "/strings";
   private static final String WINDOW_TITLE_KEY = "window_title";
 
@@ -30,9 +32,10 @@ public class Main extends Application {
     controller = fxmlLoader.getController();
     Scene scene = new Scene(root);
     stage.setTitle(bundle.getString(WINDOW_TITLE_KEY));
-    // TODO set other elements of the scene from our resources.
+    stage.getIcons().add(new Image(classLoader.getResourceAsStream(ICON_RESOURCE)));
     stage.setResizable(false);
     stage.setScene(scene);
+    stage.sizeToScene();
     stage.show();
   }
 
--- src/edu/cnm/deepdive/langton/controller/MainController.java
+++ src/edu/cnm/deepdive/langton/controller/MainController.java
@@ -7,11 +7,13 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.control.ToggleButton;
 
 public class MainController {
 
+  @FXML private Button resetter;
   @FXML private TerrainView terrainView;
   @FXML private ToggleButton runToggle;
   @FXML private Slider populationSize;
@@ -20,49 +22,65 @@ public class MainController {
   private AnimationTimer timer;
 
   @FXML
-  private void initialize(){
-    terrain = new Terrain(12, new Random());
-    timer = new AnimationTimer() {       //this is an anonymous class.
+  private void initialize() {
+    timer = new AnimationTimer() {
       @Override
       public void handle(long now) {
         terrainView.draw(terrain.getPatches());
       }
     };
+    reset();
   }
 
   @FXML
   private void toggleRun(ActionEvent actionEvent) {
-    if(runToggle.isSelected()){
+    if (runToggle.isSelected()) {
       start();
-    }else{
+    } else {
       stop();
     }
   }
 
-  private void start(){
+  private void start() {
     running = true;
+    resetter.setDisable(true);
     timer.start();
     new Runner().start();
   }
 
-  public void stop(){
+  public void stop() {
     runToggle.setDisable(true);
-    running=false;
+    running = false;
     timer.stop();
   }
 
-  private class Runner extends Thread{   //this is a nested class
+  @FXML
+  private void reset() {
+    terrain = new Terrain((int) populationSize.getValue(), new Random());
+    terrain.tick();
+    terrainView.draw(terrain.getPatches());
+  }
+
+  private class Runner extends Thread {
+
     @Override
-    public void run() { //documentation tells us there's no need for super bc Runnable is used
-      while(running){
-        terrain.tick();
+    public void run() {
+      while (running) {
+        for (int i = 0; i < 10; i++) {
+          terrain.tick();
+        }
         try {
           Thread.sleep(1);
         } catch (InterruptedException e) {
           // DO NOTHING! GET ON WITH YOUR LIFE!
         }
       }
-      Platform.runLater(() -> runToggle.setDisable(false));
+      Platform.runLater(() -> {
+        runToggle.setDisable(false);
+        resetter.setDisable(false);
+      });
     }
+
   }
+
 }
